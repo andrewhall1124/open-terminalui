504b7afb246ead64070aa528d26fd41fc8ad2214
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-12-04 13:51:34 -0700

    Added loading indicator

--- src/open_terminalui/app.py
+++ src/open_terminalui/app.py
@@ -12,29 +12,41 @@ class OpenTerminalUI(App):
         yield Header()
         with Vertical(id="container"):
             yield Markdown(id="output")
+            yield Markdown(id="loading_indicator")
             yield Input(type="text", id="input")
         yield Footer()
 
     @on(Input.Submitted, "#input")
     def handle_input_submission(self) -> None:
+        # Select input widget
         input_widget = self.query_one("#input", Input)
-        content = input_widget.value
-        if content.strip():
-            input_widget.clear()
-            self.stream_ollama_response(content)
 
-    @work(exclusive=True)
-    async def stream_ollama_response(self, content: str) -> None:
+        # Parse input content
+        content = input_widget.value.strip()
+
+        # Clear input
+        input_widget.clear()
+
+        # Stream ollama response
+        self.stream_ollama_response(content)
+
+    @work(exclusive=True, thread=True)
+    def stream_ollama_response(self, content: str) -> None:
         messages = [{"role": "user", "content": content}]
+        output_widget = self.query_one("#output", Markdown)
+        loading_indicator_widget = self.query_one("#loading_indicator", Markdown)
 
-        markdown_widget = self.query_one("#output", Markdown)
-        markdown_widget.update("")
+        self.call_from_thread(output_widget.update, "")
+        self.call_from_thread(loading_indicator_widget.update, "Thinking...")
+
         accumulated_text = ""
         stream = chat(model="llama3.2", messages=messages, stream=True)
 
-        for chunk in stream:
+        for i, chunk in enumerate(stream):
             accumulated_text += chunk["message"]["content"]
-            markdown_widget.update(accumulated_text)
+            self.call_from_thread(output_widget.update, accumulated_text)
+            if i == 0:
+                self.call_from_thread(loading_indicator_widget.update, "")
 
     def action_toggle_dark(self) -> None:
         self.theme = (
